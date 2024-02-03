3b619c0558fb7d967a72c4a5e01c031140b80c9d
deRYtIOn <115622895+deRYtIOn@users.noreply.github.com>
2024-02-03 18:27:04 +0530

    Final

---
+++ Event-Facilitation/src/config/config.js
@@ -0,0 +1,5 @@
+module.exports={
+    REACT_APP_MONGO_URI:"mongodb+srv:sumo:1234@cluster0.qib41ri.mongodb.net/?retryWrites=true&w=majority",
+    REACT_APP_STRIPE_PRIVATE:"sk_test_51Lx3J0SC2xXpnqlIHbyG4A4c62EgV7FA8g0ki0nhdJViwKI9pvInkzT6dxCURU0yz5mDcsUnfarGbyrwQVqJJ4Ij008fxcfbHV",
+    REACT_APP_STRIPE_PUBLIC:"pk_test_51Lx3J0SC2xXpnqlILiMvtKCySQ6qc4YHbA30HNk3WbUzMvbys5QcmxKQwfS61FcOQ8vpwEn6lDZL7uILoi8zyqtv00942zJGRA"
+}
---
+++ Event-Facilitation/src/screens/LandingScreen.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import AOS from "aos";
+
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init({
+  duration: 2000,
+});
+
+function LandingScreen() {
+  return (
+    <div className="row landing">
+      <div className="col-md-12 text-center">
+        <h2 data-aos="zoom-in" style={{ color: "white", fontSize: "100px" }}>
+          EVENT BOOKING
+        </h2>
+        <h1 data-aos="zoom-out" style={{ color: "white" }}>
+          Welcome to our website!
+        </h1>
+        <Link to="/home">
+          <button className="btn btn-primary landingBtn">Get Started</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default LandingScreen;
